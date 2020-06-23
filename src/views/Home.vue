7f5ce10a5d4570b34cf5ddf5aaae28dcc5057cf2
Seweryn W <wadowski17@gmail.com>
2020-06-23 16:03:32 -0400

    default set

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">Hello W!</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {},
+
+  data() {
+    return {
+      baseURL: 'http://157.245.138.232:9091/api/v1/test/superheroes',
+      list: []
+    }
+  },
+
+  computed: {},
+  methods: {
+    async getSuperheroList() {
+      const res = await axios.get(this.baseURL)
+      console.log('response', res)
+    }
+  },
+
+  mounted() {
+    this.getSuperheroList()
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  background-color: gray;
+  height: 100vh;
+}
+</style>
